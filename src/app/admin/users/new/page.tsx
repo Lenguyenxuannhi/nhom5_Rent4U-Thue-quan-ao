9828e00eb4fe00c11daa99cbce077e4658d4d345
khai <khai258114@gmail.com>
2026-04-20 10:48:47 +0700

    feat: add user role filter on users list

---
+++ src/app/admin/users/new/page.tsx
@@ -0,0 +1,10 @@
+import UserForm from '@/components/admin/UserForm'
+
+export default function NewUserPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">Create User</h1>
+      <UserForm />
+    </div>
+  )
+}
