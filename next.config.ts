8fcc10b082ee81f83b03c5f36f9591ecf231000a
hao <thienhaon4@gmail.com>
2026-04-21 16:56:15 +0700

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/Rent4UNext',
+  assetPrefix: '/Rent4UNext/',
+  images: {
+    unoptimized: true,
+  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
